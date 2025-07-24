02897541438a9c16f75de8122209e777d47f16dd
uAnnaah <annalyabonomini@gmail.com>
2025-07-23 21:18:28 -0300

    atualizações

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,20 +1,23 @@
 function cadastrarUsuario() {
   const nome = document.getElementById("nome").value;
-  const cidade = document.getElementById("cidade").value;
+  const cidade = document.getElementById("cidades").value;
   const email = document.getElementById("email").value;
   const senha = document.getElementById("senha").value;
   const mensagem = document.getElementById("mensagem");
+
+  console.log(email, senha);
 
   if (!nome || !cidade || !email || !senha) {
     mensagem.textContent = "Por favor, preencha todos os campos.";
     mensagem.className = "erro";
     return;
   }
-
-  localStorage.setItem("emailCadastro", email);
-  localStorage.setItem("senhaCadastro", senha);
+else{
   mensagem.textContent = "Conta criada com sucesso!";
   mensagem.className = "sucesso";
+}
+  localStorage.setItem("emailCadastro", email);
+  localStorage.setItem("senhaCadastro", senha);
 
   setTimeout(() => {
     window.location.href = "index.html";
